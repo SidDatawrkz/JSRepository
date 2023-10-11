8c3c71f247f54e7697d9fc181d712781227b2370
SidDatawrkz <147612116+SidDatawrkz@users.noreply.github.com>
2023-10-11 16:52:12 +0530

    Delete getparam.js

--- getparam.js
+++
@@ -1,9 +0,0 @@
-      function getparam(key){
-          //fetching param key-value passed: https://www.sitepoint.com/get-url-parameters-with-javascript/
-          const queryString = window.location.search;
-          console.log(queryString);
-          const urlParams = new URLSearchParams(queryString);
-          const value = urlParams.get(key);
-          console.log(value);
-          return value;
-      }
